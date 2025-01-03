c3288064aed0b6cc2ee7a586a666febce926532b
Sajid Ahamed <109978894+Sajidcodecrack@users.noreply.github.com>
2025-01-03 23:44:23 +0600

    Update database.sql
    Registration page quries added

--- Backend/database.sql
+++ Backend/database.sql
@@ -1,11 +1,25 @@
 CREATE DATABASE Research_Hub;
 
 USE Research_Hub;
+
 DROP TABLE login_db;
 
-CREATE TABLE login_db (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
+CREATE TABLE registration_db (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    age INT NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE login_db (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
 SELECT * FROM login_db;
+SELECT * FROM registration_db;
+SELECT name, email
+FROM registration_db
+WHERE age > 45;
